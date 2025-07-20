87dd25b0b00ce7bb98f3930294dbf8705903b1e3
Jéssica Trigo <jessica.trigo05@icloud.com>
2025-07-20 14:10:33 +0200

    Created new project

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  import viteLogo from "/vite.svg";
+</script>
+
+<main>
+  <h1>project</h1>
+</main>
+
+<style>
+
+</style>
